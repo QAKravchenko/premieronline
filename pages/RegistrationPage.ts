3cce46726d30040a5fbe6eea271ef0014e2ecd28
Svyatoslav <qakravchenko@gmail.com>
2026-09-03 16:37:13 +0300

    Registration with positive and negative cases

--- pages/RegistrationPage.ts
+++ pages/RegistrationPage.ts
@@ -1,5 +1,13 @@
-import {expect, Locator, Page} from '@playwright/test';
+import { expect, Locator, Page } from '@playwright/test';
 import { Header } from './components/Header';
+
+export interface RegistrationData {
+    email: string;
+    firstName: string;
+    lastName: string;
+    password: string;
+    repeatPassword: string;
+}
 
 export class RegistrationPage {
 
@@ -22,6 +30,18 @@ export class RegistrationPage {
     readonly continueButton: Locator;
 
     readonly checkYourEmailMessageBlock: Locator;
+
+    readonly errorMessageEmptyEmailField: Locator;
+
+    readonly errorMessageIncorrectEmailField: Locator;
+
+    readonly errorMessageFirstNameField: Locator;   
+
+    readonly errorMessageLastNameField: Locator;
+
+    readonly errorMessagePasswordField: Locator;
+
+    readonly errorMessageDifferentPasswords: Locator;
 
     readonly header: Header;
 
@@ -49,64 +69,139 @@ export class RegistrationPage {
 
         this.checkYourEmailMessageBlock = page.locator('uk-alert-success');
 
+        this.errorMessageEmptyEmailField = page.locator('.uk-alert-danger').filter({ hasText: 'Please enter your email address.' });
+
+        this.errorMessageIncorrectEmailField = page.locator('.uk-alert-danger').filter({ hasText: 'Your email address is incorrect.' });
+
+        this.errorMessageFirstNameField = page.locator('.uk-alert-danger').filter({ hasText: 'Please enter your first name.' });
+
+        this.errorMessageLastNameField = page.locator('.uk-alert-danger').filter({ hasText: 'Please enter your last name.' });
+
+        this.errorMessagePasswordField = page.locator('.uk-alert-danger').filter({ hasText: 'Please enter a password.' });
+        
+        this.errorMessageDifferentPasswords = page.locator('.uk-alert-danger').filter({ hasText: "Your passwords don't match." });
+
         this.header = new Header(page);
 
     }
 
-    async fillEmailField() {
+    async checkPasswordVisibilityToggle() {
+        await expect(this.passwordField).toHaveAttribute('type', 'password');
+        await this.eyeIconFirst.click();
+        await expect(this.passwordField).toHaveAttribute('type', 'text');
+        await expect(this.repeatPasswordField).toHaveAttribute('type', 'password');
+        await this.eyeIconLast.click();
+        await expect(this.repeatPasswordField).toHaveAttribute('type', 'text');
+    }
+
+    async register(data: RegistrationData): Promise<void> {
+        await this.emailField.fill(data.email);
+        await this.firstNameField.fill(data.firstName);
+        await this.lastNameField.fill(data.lastName);
+        await this.passwordField.fill(data.password);
+        await this.repeatPasswordField.fill(data.repeatPassword);
+        await expect(this.emailField).toHaveValue(data.email);
+        await expect(this.firstNameField).toHaveValue(data.firstName);
+        await expect(this.lastNameField).toHaveValue(data.lastName);
+        await expect(this.passwordField).toHaveValue(data.password);
+        await expect(this.repeatPasswordField).toHaveValue(data.repeatPassword);
+        await this.clickContinueButton();
+    }
+
+    async checkEmailField() {
         await expect(this.emailField).toBeVisible();
         await expect(this.emailField).toBeEnabled();
         await expect(this.emailField).toHaveAttribute('placeholder', 'Email: we send your confirmations and receipts here.');
-        await (this.emailField).fill('qa@gmail.com');
-        await expect(this.emailField).toHaveValue('qa@gmail.com');
+        //await (this.emailField).fill(email);
+        //await expect(this.emailField).toHaveValue();
     }
 
-    async fillFirstNameField() {
+    async checkFirstNameField() {
         await expect(this.firstNameField).toBeVisible();
         await expect(this.firstNameField).toBeEnabled();
         await expect(this.firstNameField).toHaveAttribute('placeholder', 'First Name');
-        await (this.firstNameField).fill('QA');
-        await expect(this.firstNameField).toHaveValue('QA');
+        //await (this.firstNameField).fill(firstName);
+        //await expect(this.firstNameField).toHaveValue(firstName);
     }
 
-    async fillLastNameField() {
+    async checkLastNameField() {
         await expect(this.lastNameField).toBeVisible();
         await expect(this.lastNameField).toBeEnabled();
         await expect(this.lastNameField).toHaveAttribute('placeholder', 'Last Name');
-        await (this.lastNameField).fill('Test');
-        await expect(this.lastNameField).toHaveValue('Test');
+        //await (this.lastNameField).fill(lastName);
+        //await expect(this.lastNameField).toHaveValue(lastName);
     }
 
-    async fillPasswordField() {
+   
+    async checkPasswordField() {
         await expect(this.passwordField).toBeVisible();
         await expect(this.passwordField).toBeEnabled();
         await expect(this.passwordField).toHaveAttribute('placeholder', 'Password (8 characters alphanumeric)');
-        await (this.passwordField).fill('testing');
-        await expect(this.passwordField).toHaveValue('testing');
-        await expect(this.passwordField).toHaveAttribute('type', 'password');
-        await (this.eyeIconFirst).click();
-        await expect(this.passwordField).toHaveAttribute('type', 'text');
+        //await (this.passwordField).fill(password);
+        //await expect(this.passwordField).toHaveValue(password);
+        //await expect(this.passwordField).toHaveAttribute('type', 'password');
+        //await this.eyeIconFirst.click();
+        //await expect(this.passwordField).toHaveAttribute('type', 'text');
     }
 
-    async fillRepeatPasswordField() {
+    async checkRepeatPasswordField() {
         await expect(this.repeatPasswordField).toBeVisible();
         await expect(this.repeatPasswordField).toBeEnabled();
         await expect(this.repeatPasswordField).toHaveAttribute('placeholder', 'Repeat Password');
-        await (this.repeatPasswordField).fill('testing');
-        await expect(this.repeatPasswordField).toHaveValue('testing');
-        await expect(this.repeatPasswordField).toHaveAttribute('type', 'password');
-        await (this.eyeIconLast).click();
-        await expect(this.repeatPasswordField).toHaveAttribute('type', 'text');
+        //await (this.repeatPasswordField).fill(repeatPassword);
+        //await expect(this.repeatPasswordField).toHaveValue(repeatPassword);
+        //await expect(this.repeatPasswordField).toHaveAttribute('type', 'password');
+        //await this.eyeIconLast.click();
+        //await expect(this.repeatPasswordField).toHaveAttribute('type', 'text');
     }
 
     async clickContinueButton() {
         await expect(this.continueButton).toBeVisible();
         await expect(this.continueButton).toBeEnabled();
         await expect(this.continueButton).toHaveAttribute('type', 'submit');
-        await (this.continueButton).click();
+        await this.continueButton.click();
+    }    
+
+    async checkSuccessfulRegistration(value: string) {    
         await expect(this.page).toHaveURL('https://www.premieronline.com/create_profile.php');
         await expect(this.checkYourEmailMessageBlock).toBeVisible();
         await expect(this.checkYourEmailMessageBlock).toBeEnabled();
-        await expect(this.checkYourEmailMessageBlock).toHaveText("Please check your email for your Activation Button. Click it and you'll come right back here and be able to start registering immediately.");
+        await expect(this.checkYourEmailMessageBlock).toHaveText(value);
+    }
+
+    async checkErrorMessageEmptyEmailField() {
+        await expect(this.errorMessageEmptyEmailField).toBeVisible();
+        await expect(this.errorMessageEmptyEmailField).toBeEnabled();
+        await expect(this.errorMessageEmptyEmailField).toContainText('Please enter your email address.');
+    }
+
+    async checkErrorMessageIncorrectEmailField() {
+        await expect(this.errorMessageIncorrectEmailField).toBeVisible();
+        await expect(this.errorMessageIncorrectEmailField).toBeEnabled();
+        await expect(this.errorMessageIncorrectEmailField).toContainText('Your email address is incorrect.');
+    }
+
+    async checkErrorMessageEmptyFirstNameField() {
+        await expect(this.errorMessageFirstNameField).toBeVisible();
+        await expect(this.errorMessageFirstNameField).toBeEnabled();
+        await expect(this.errorMessageFirstNameField).toContainText('Please enter your first name.');
+    }
+
+    async checkErrorMessageEmptyLastNameField() {
+        await expect(this.errorMessageLastNameField).toBeVisible();
+        await expect(this.errorMessageLastNameField).toBeEnabled();
+        await expect(this.errorMessageLastNameField).toContainText('Please enter your last name.');
+    }
+
+    async checkErrorMessageEmptyPasswordField() {
+        await expect(this.errorMessagePasswordField).toBeVisible();
+        await expect(this.errorMessagePasswordField).toBeEnabled();
+        await expect(this.errorMessagePasswordField).toContainText('Please enter a password.');
+    }
+
+    async checkErrorMessageDifferentPasswords() {
+        await expect(this.errorMessageDifferentPasswords).toBeVisible();
+        await expect(this.errorMessageDifferentPasswords).toBeEnabled();
+        await expect(this.errorMessageDifferentPasswords).toContainText("Your passwords don't match.");
     }
 }
--- tests/registrationNew.spec.ts
+++ tests/registrationNew.spec.ts
@@ -4,39 +4,94 @@ import { RegistrationPage } from "../pages/RegistrationPage";
 
 
 test.describe('Check registration', () => {
-        let header: Header;
-        let registrationPage: RegistrationPage;
-        test.beforeEach(async ({ page }) => {
-            header = new Header(page);
-            registrationPage = new RegistrationPage(page);
+    let header: Header;
+    let registrationPage: RegistrationPage;
+    const emailData = 'qa@gmail.com';
+    const firstNameData = 'QA';
+    const lastNameData = 'Test';
+    const passwordData = 'testing1';
+    const repeatPasswordData = 'testing1';
+    const successfulMessage = "Please check your email for your Activation Button. Click it and you'll come right back here and be able to start registering immediately.";
+    test.beforeEach(async ({ page }) => {
+        header = new Header(page);
+        registrationPage = new RegistrationPage(page);
         await page.goto('/');
         await header.registerLink.click();
         await expect(page).toHaveURL('https://www.premieronline.com/action/register');
-    })
-
-test('POSITIVE - Check successful registration', async () => {
-        await test.step('Type email in the "Email" field', async () => {
-            await registrationPage.fillEmailField();
-        })
-
-        await test.step('Type first name in the "First Name" field', async () => {
-            await registrationPage.fillFirstNameField();
-        })
-
-        await test.step('Type last name in the "Last Name" field', async () => {
-            await registrationPage.fillLastNameField();
-        })
-
-        await test.step('Type password in the "Password" field', async () => {
-            await registrationPage.fillPasswordField();
-        })
-
-        await test.step('Re-type password in the "Repeat password" field', async () => {
-            await registrationPage.fillRepeatPasswordField();
-        })
-
-        await test.step('Click on "Continue" button', async () => {
-            await registrationPage.clickContinueButton();
-        })
     });
-});           
+
+    test('Check initial state of registration fields', async () => {
+        await test.step('Verify all form fields are present', async () => {
+            await registrationPage.checkEmailField();
+            await registrationPage.checkFirstNameField();
+            await registrationPage.checkLastNameField();
+            await registrationPage.checkPasswordField();
+            await registrationPage.checkRepeatPasswordField();
+        });
+    });
+
+    test('POSITIVE - Check successful registration', async () => {
+        await test.step('Check Password Visibility Toggle', async () => {
+            await registrationPage.checkPasswordVisibilityToggle();
+        })
+
+        await test.step('Fill registration fields with valid data', async () => {
+            await registrationPage.register({
+                email: emailData,
+                firstName: firstNameData,
+                lastName: lastNameData,
+                password: passwordData,
+                repeatPassword: repeatPasswordData
+            });
+        })
+
+        await test.step('Check successful message', async () => {
+            await registrationPage.checkSuccessfulRegistration(successfulMessage);
+        });
+    });
+
+    test('NEGATIVE - Check registration with empty fields', async () => {
+        await test.step('Leave all registration fields empty and click on "Continue" button', async () => {
+            await registrationPage.clickContinueButton();
+        });
+
+        await test.step('Check error messages for empty fields', async () => {
+            await registrationPage.checkErrorMessageEmptyEmailField();
+            await registrationPage.checkErrorMessageEmptyFirstNameField();
+            await registrationPage.checkErrorMessageEmptyLastNameField();
+            await registrationPage.checkErrorMessageEmptyPasswordField();
+        });
+    });
+
+    test('NEGATIVE - Check registration with invalid email', async () => {
+        await test.step('Fill registration fields with invalid email and click on "Continue" button', async () => {
+            await registrationPage.register({
+                email: 'test@test',
+                firstName: firstNameData,
+                lastName: lastNameData,
+                password: passwordData,
+                repeatPassword: repeatPasswordData
+            });
+        });
+
+        await test.step('Check error message for invalid email', async () => {
+            await registrationPage.checkErrorMessageIncorrectEmailField();
+        });
+    });
+
+    test('NEGATIVE - Check registration with different passwords', async () => {
+        await test.step('Fill registration fields with different passwords and click on "Continue" button', async () => {
+            await registrationPage.register({
+                email: emailData,
+                firstName: firstNameData,
+                lastName: lastNameData,
+                password: passwordData,
+                repeatPassword: 'differentPassword'
+            });
+        });
+
+        await test.step('Check error message for different passwords', async () => {
+            await registrationPage.checkErrorMessageDifferentPasswords();
+        });
+    });
+});       
